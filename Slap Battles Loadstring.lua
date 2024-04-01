39fb08bab56861371ac19e968f58a21e4b4dce4c
robloxsuckslikereallydude <120487795+robloxsuckslikereallydude@users.noreply.github.com>
2024-03-31 19:54:32 -0500

    Update Slap Battles Loadstring.lua

--- Slap Battles Loadstring.lua
+++ Slap Battles Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/robloxsuckslikereallydude/1-Line-Scripts/main/Slap%20Battles.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/robloxsuckslikereallydude/1-Line-Scripts/main/Slap%20Battles")))()
